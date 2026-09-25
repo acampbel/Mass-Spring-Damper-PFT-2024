9e20fdc6523578eeaf3b962d9130c085fcc8ff43
Andy Campbell <47190204+acampbel@users.noreply.github.com>
2026-09-25 11:44:30 -0400

    Update version string format in buildfile.m
    Removed timestamp from version string in buildfile.m.

--- buildfile.m
+++ buildfile.m
@@ -141,7 +141,7 @@ function toolboxTask(context,version)
 
 arguments
     context
-    version string = "2.3.13." + string(posixtime(datetime('now')) * 1e6);
+    version string = "2.3.13"
 end
 
 outputFile = context.Task.Outputs.paths;
